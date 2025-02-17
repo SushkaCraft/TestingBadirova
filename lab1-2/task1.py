8bc9d3774049a6a29ab10651ffe2fc3221e4a40c
SushkaCraft <102476271+SushkaCraft@users.noreply.github.com>
2025-02-17 14:01:02 +0300

    Fix Func is_valid()

--- lab1-2/task1.py
+++ lab1-2/task1.py
@@ -1,7 +1,7 @@
 import math 
  
-def is_valid(a, b, c): 
-    return a > 0 and b > 0 and c > 0 
+def is_valid(a, b, c):
+    return (a > 0 and b > 0 and c > 0) and (a + b > c) and (a + c > b) and (b + c > a)
  
 def triangle_type(a, b, c): 
     if a == b == c: 
